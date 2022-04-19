7a845da25e9edfd202b9dca0e76585e0387a21d5
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-04-19 18:59:43 +0900

    useState&JSX exam

---
+++ study_reactjs/reactexam1/src/App.js
@@ -0,0 +1,15 @@
+//import './App.css';
+import MyHeader from './MyHeader';
+import Counter from './Counter';
+
+
+function App() {
+  return (
+    <div>
+      <MyHeader />
+        <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ study_reactjs/reactexam1/src/Counter.js
@@ -0,0 +1,23 @@
+import React,{useState} from "react";
+
+const Counter = ()=>{
+
+    //요구 조건 0에서 출발, 1씩 증가, 1씩 감소, count 상태
+    const [count, setCount] = useState(0);
+
+    const onIncrease = () =>{
+        setCount(count + 1);
+    }
+    const onDecrease = ()=>{
+        setCount(count -1 );
+    }
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
+    )    
+}
+
+export default Counter;
