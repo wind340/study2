6ece079244ee5aa80b9ffdd537ebe21d55e3d893
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-28 23:48:50 +0900

    interface 적용 js변환

---
+++ typescript/interface.js
@@ -0,0 +1,27 @@
+//타입을 지정
+/*
+type Player = {
+  nickname:string,
+  healthBar:number
+}
+
+const chul :Player = {
+  nickname: "chul",
+  healthBar: 10
+}
+type Food = string;
+const godkimchi:Food = "delicious"
+*/
+var chul = {
+    nickname: "jake",
+    team: "red",
+    health: 10,
+};
+var chul2 = {
+    name: "jake",
+};
+var jungchul = {
+    name: "jake",
+    lastName: "c",
+    health: 99,
+};
