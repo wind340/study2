153ca6d2521dc7f234166be2218806f60a653ff2
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-29 23:51:12 +0900

    정의 파일을 구현하지 않았을때 @ts-check를 통해 검사 초기화 구현

--- typescript/typechain/src/myPackage.js
+++ typescript/typechain/src/myPackage.js
@@ -1,7 +1,20 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config  //object형의 parameter가와야함
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
   return true;
 }
 
+/**
+ *
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
