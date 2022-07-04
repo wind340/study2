489bac2fff6f711c848cbd1372b3267018185e80
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-04 22:44:51 +0900

    10871 백준코테

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -3,25 +3,28 @@ package codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		//2439 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-		//하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+		//10871 X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
         
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= t; i++) {
-        	for(int j = t- 1; j>=i; j--) {
-        		sb.append(" ");
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        for (int i = 0; i < n; i++) {
+        	int a = Integer.parseInt(st.nextToken());
+        	if(a<x) {
+        		sb.append(a).append(" ");
         	}
-        	for(int j= 1; j<=i; j++) {
-        		sb.append("*");
-        	}
-        	sb.append("\n");
         }
+        
         System.out.println(sb);
 	}
 }
