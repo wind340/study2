8f5f066830a1a2b85ed8e313a4244c27beeefea2
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-04 19:55:50 +0900

    2438 백준코테

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -6,20 +6,18 @@ import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		//11022  각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. 
-		//x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+		//2438 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
         int t = Integer.parseInt(br.readLine());
         for (int i = 1; i <= t; i++) {
-            String str = br.readLine();
-            sb.append("Case #").append(i).append(": ")
-	            .append(str.charAt(0)).append(" + ").append(str.charAt(2))
-	            .append(" = ").append(str.charAt(0)-'0'+str.charAt(2)-'0').append('\n');
+        	for(int j = 1; j<=i; j++) {
+        		sb.append("*");
+        	}
+        	sb.append("\n");
         }
         System.out.println(sb);
-
 	}
 }
