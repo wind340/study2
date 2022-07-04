fd44e9d16a5366403505d881615187d2b1fc36cf
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-05 00:37:48 +0900

    10951 백준코테

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -7,24 +7,17 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		//10871 X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		StringBuilder sb = new StringBuilder();
-        
-        int n = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        
-        st = new StringTokenizer(br.readLine(), " ");
-        
-        for (int i = 0; i < n; i++) {
-        	int a = Integer.parseInt(st.nextToken());
-        	if(a<x) {
-        		sb.append(a).append(" ");
-        	}
-        }
-        
-        System.out.println(sb);
+		String str = "";
+		
+		while((str= br.readLine())!=null && !str.isEmpty()){
+			StringTokenizer st = new StringTokenizer(str, " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a+b).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
