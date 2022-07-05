95ab22ad966572bfd88c261678e669a3eab093eb
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-05 16:24:42 +0900

    2562 백준코테 배열

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -7,23 +7,29 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		// 10818 N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+		// 2562 9개의 정수가 주어진다.  최댓값과 몇번째 수인지 구하는 프로그램을 작성하시오.
+		//첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
 		
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        int min = 1000000;
-        int max = -1000000;
-        st = new StringTokenizer(br.readLine());
-        for (int nIndex = 0; nIndex < n; nIndex++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (min > num) min = num;
-            if (max < num) max = num;
+
+        int[] array = new int[9];
+
+        for(int i =0; i<9; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	array[i] = Integer.parseInt(st.nextToken());	
         }
+        
+        int max = 0;
+        int index = 0;
+        for(int i=0; i<array.length; i++) {
+        	if(array[i]>max) {
+        		max=array[i];
+        		index = i+1;
+        	};
+       }
         StringBuilder sb = new StringBuilder();
-        sb.append(min).append(' ').append(max);
+        sb.append(max).append('\n').append(index);
         System.out.print(sb);
     }
 }
