d29aad6912ecc2249ee0ec2d4539359fa3283d11
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-05 12:52:11 +0900

    1110 백준코테(반복문)
    do while문

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -7,17 +7,48 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
+/*1110 문제
+ * 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 
+ * 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 
+ * 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 다음 예를 보자.
+ * 26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 
+ * 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
+ * 위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
+ * N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.*/
 
+//		첫째 줄에 N이 주어진다. N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String str = "";
-		
-		while((str= br.readLine())!=null && !str.isEmpty()){
-			StringTokenizer st = new StringTokenizer(str, " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			sb.append(a+b).append("\n");
+		int n = Integer.parseInt(br.readLine());
+		int count=0;
+
+//		do while문
+//      int result = number;
+//      do {
+//          result = ((result % 10) * 10) + (((result / 10) + (result % 10)) % 10);
+//          count++;
+//      } while (number != result);
+//
+//      System.out.println(count);
+
+//		while문으로만 하드 코딩해보기
+		int a,b;
+		if(n<10) {
+			a= 0;
+			b= n;
+		}else {
+			a=n/10;
+			b=n%10;
 		}
-		System.out.print(sb);
+		int sum = a+b;
+	
+		while(true){
+			a=b;
+			b= sum%10;
+			sum = a+b;
+			count++;
+			if(a*10+b==n)break;
+		}
+		System.out.print(count);
+
 	}
 }
