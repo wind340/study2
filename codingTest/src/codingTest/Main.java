7ef371453fda7c516a0209ea7d3fbfb2d9cebc6c
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-06 23:55:16 +0900

    Update Main.java

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -3,9 +3,7 @@ package codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
