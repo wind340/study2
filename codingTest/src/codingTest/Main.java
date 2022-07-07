3695d549f2f798be9021b90ec70031fbe5d43c53
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-07 23:32:47 +0900

    1546 백준코테 (배열)

--- codingTest/src/codingTest/Main.java
+++ codingTest/src/codingTest/Main.java
@@ -1,23 +1,32 @@
 package codingTest;
 
+import java.util.Arrays;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		// 3052 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 
-		// 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
-		// 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+	public static void main(String[] args) throws IOException {
 		
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        HashSet<Integer> h = new HashSet<Integer>();
-
-        for(int i=0; i<10; i++) {
-        	h.add(Integer.parseInt(br.readLine())%42);
-        }
-        System.out.println(h.size());
+		//1546 입력받은 숫자중 가장큰수를 기준으로 나눈다음 100을 곱하여 새로운 평균값을 구하라
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		double arr[] = new double[Integer.parseInt(br.readLine())];
+        
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Double.parseDouble(st.nextToken());
+		}
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[arr.length - 1]) * 100);
+		}
+		System.out.print(sum / arr.length);
 	}
 }
